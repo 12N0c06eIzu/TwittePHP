5b6edab3e4d83c81ad289a9cabd880aca8ab8e8b
Rikuto Hayashi <ritu6811@gmail.com>
2019-08-26 22:42:28 +0900

    first commit

---
+++ join/check.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>よくわかるPHPの教科書</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<form action="" method="post">
+    <dl>
+        <dt>ニックネーム</dt>
+        <dd>
+        <?php echo htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES); ?>
+        </dd>
+        <dt>メールアドレス</dt>
+        <dd>
+        <?php echo htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES); ?>
+        </dd>
+        <dt>パスワード</dt>
+        <dd>
+        【表示されません】
+        </dd>
+        <dt>写真など</dt>
+        <dd>
+        </dd>
+    </dl>
+    <div><a href="index.php?action=rewrite">&laquo;&nbsp;書き直す</a>|<input type="submit" value="登録する"></div>
+</form>
+<?php
+/* ここに、PHPのプログラムを記述します */
+?>
+</main>
+</body>
+</html>
---
+++ template.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>よくわかるPHPの教科書</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">よくわかるPHPの教科書</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<p>次のフォームに必要事項をご記入ください</p>
+<form action="" method="post" enctype="multipart/form-data">
+    <dl>
+        <dt>ニックネーム<span class="required">必須</span></dt>
+        <dd><input type="text" name="" size="35" maxlength="255"></dd>
+        <dt>メールアドレス<span class="required">必須</span></dt>
+        <dd><input type="text" name="email" size="35" maxlength="255"></dd>
+        <dt>パスワード<span class="required">必須</span></dt>
+        <dd><input type="password" name="password" size="10" maxlength="20"></dd>
+        <dt>写真など</dt>
+        <dd><input type="file" name="image" size="35"></dd>
+    </dl>
+    <div><input type="submit" value="入力内容を確認する"></div>
+</form>
+<?php
+/* ここに、PHPのプログラムを記述します */
+?>
+</main>
+</body>
+</html>
